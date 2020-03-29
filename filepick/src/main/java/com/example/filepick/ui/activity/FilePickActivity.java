8ac86aea954221953b80f0f46ced51612b82d99c
user <jha.saket350@gmail.com>
2020-03-29 11:06:23 +0530

    FilePickActivity.java - Added Transparent Activity for File picker. colors.xml strings.xml styles.xml - Added colors file strings file and styles file with transparent theme. AndroidManifest.xml - Added minor changes and transparent theme in FilePickActivity.

---
+++ filepick/src/main/java/com/example/filepick/ui/activity/FilePickActivity.java
@@ -0,0 +1,17 @@
+package com.example.filepick.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by SAKET on 29/03/2020
+ */
+public class FilePickActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
